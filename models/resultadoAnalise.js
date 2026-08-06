db26a8698b1a251631d9ac0b2a97857dbc2a4060
gustavovalentindeandrade-byte <gustavovalentindeandrade@gmail.com>
2026-08-06 12:02:25 -0300

    Update resultadoAnalise.js
    novo código com carteira e macro setor

--- models/resultadoAnalise.js
+++ models/resultadoAnalise.js
@@ -1,5 +1,16 @@
 export class ResultadoAnalise {
-    constructor({ perfil, macroSetor, carteira, possuiIndustrial, motivacaoIndustrial, cnaesIndustriaisLista, todasAnalisadas }) {
+    constructor({ 
+        perfil, 
+        macroSetor, 
+        carteira, 
+        possuiIndustrial, 
+        motivacaoIndustrial, 
+        cnaesIndustriaisLista, 
+        todasAnalisadas,
+        // NOVO: Parâmetro opcional para armazenar os dados do banco
+        analiseAvancada 
+    }) {
+        // Campos originais preservados
         this.perfil = perfil;
         this.macroSetor = macroSetor;
         this.carteira = carteira;
@@ -7,5 +18,17 @@ export class ResultadoAnalise {
         this.motivacaoIndustrial = motivacaoIndustrial;
         this.cnaesIndustriaisLista = cnaesIndustriaisLista;
         this.todasAnalisadas = todasAnalisadas;
+        
+        // NOVO: Encapsula a classificação detalhada de Carteiras e Macro Setores
+        // O fallback garante retrocompatibilidade com históricos antigos salvos.
+        this.analiseAvancada = analiseAvancada || {
+            empresaIndustrial: "NÃO",
+            totalCnaes: todasAnalisadas ? todasAnalisadas.length : 0,
+            qtdIndustrial: 0,
+            qtdNaoIndustrial: 0,
+            carteiras: [],
+            macroSetores: [],
+            resultados: []
+        };
     }
 }
